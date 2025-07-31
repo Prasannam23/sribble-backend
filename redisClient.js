c86b6527e92e8c89862066f7079c0014128f262f
Prasannam Tiwari <prasannamtiwari23@gmail.com>
2025-07-31 11:49:31 +0530

    error fixed

--- redisClient.js
+++ redisClient.js
@@ -4,7 +4,7 @@ import { Redis } from '@upstash/redis';
 
 const redisClient = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
-  token: 'process.env.UPSTASH_REDIS_REST_TOKEN',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 
