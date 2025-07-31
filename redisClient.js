c62a0af12b3930096f93c1b2c89a00d38e4d04aa
Prasannam Tiwari <prasannamtiwari23@gmail.com>
2025-07-31 11:03:40 +0530

    upstash integrated

--- redisClient.js
+++ redisClient.js
@@ -1,19 +1,11 @@
-// redisClient.js
-import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const redisClient = createClient({
-  url: process.env.UPSTASH_REDIS_REST_URL  ,
-  token:'UPSTASH_REDIS_REST_TOKEN',
+import { Redis } from '@upstash/redis';
+
+const redisClient = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: 'process.env.UPSTASH_REDIS_REST_TOKEN',
 });
-
-redisClient.on('error', (err) => {
-  console.error('Redis Client Error', err);
-});
-
-(async () => {
-  await redisClient.connect();
-  console.log('Redis connected');
-})();
 
 export default redisClient;
-process.env.REDIS_URL
